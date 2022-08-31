78b4c328dc25c0ddc34f294dba00413008986e61
Nazim Rizvic <nazim.rizvic@me.com>
2022-08-31 13:01:28 +1000

    create country class to define country object that will hold alculated data

--- countryClasses.py
+++ countryClasses.py
@@ -1,2 +1,11 @@
-from calcFunctions import *
+
+class Country:
+
+    def __init__(self, name, avgInflationRate, avgPopulation, inflationDataByYear, populationDataByYear):
+        self.name = name
+        self.avgInflationRate = avgInflationRate
+        self.avgPopulation = avgPopulation
+        self.inflationDataByYear = inflationDataByYear
+        self.populationDataByYear = populationDataByYear
+
 
