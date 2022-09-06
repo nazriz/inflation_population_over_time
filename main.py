21dd469cfd371b5fe26c9c6c03e5fbfe4ee2c82b
Nazim Rizvic <nazim.rizvic@me.com>
2022-09-06 10:25:25 +1000

    added function for dataframe output that will present results in csv friendly format

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from countryClasses import *
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-inflation_df = pd.read_csv ('inflation_data.csv')
+# inflation_df = pd.read_csv ('inflation_data.csv')
 population_df = pd.read_csv('population_data.csv')
 total_world_pop_df = pd.read_csv('world_population_by_year.csv')
-
+headline_inflation_df = pd.read_csv('headline_inflation.csv')
+bis_inflation_df = pd.read_csv('bis_inflation_data.csv')
 
 targetYears = createYearsList(1961, 2021, 1)
 
-inflationDict = averageInflationByCountryByYearRange(inflation_df, targetYears)
+# inflationDict = averageInflationByCountryByYearRange(inflation_df, targetYears)
+inflationDict = inflationByCountryByYearTwoSources(headline_inflation_df, bis_inflation_df, targetYears, "20")
 populationDict = averagePopulationByCountryByYearYange(population_df, inflationDict, targetYears)
 worldPopDict = generateWorldPopulationData(total_world_pop_df, targetYears)
+
 
 
 
@@ -25,16 +28,16 @@ def outputDataByAverage(yearRange, inflationData, populationData, worldPopulatio
             countryName = str(list(countryInflation.keys())[0])
             countryObj = Country()
             countryObj.setCountryName(countryName)
-            countryObj.setAvgInflationRate(countryInflation[countryName])
+            countryObj.setInflationRate(countryInflation[countryName])
             countryObjects[year].append(countryObj)
                 
 
         for countryPopulation in populationData[year]:
             countryName = str(list(countryPopulation.keys())[0])
-            avgPopulation = countryPopulation[countryName]
+            population = countryPopulation[countryName]
             for country in countryObjects[year]:
                 if countryName == country.name:
-                    country.setAvgPopulation(avgPopulation)
+                    country.setPopulation(population)
 
 
         worldPopObj = worldPopulation()
@@ -56,8 +59,8 @@ def outputDataByAverage(yearRange, inflationData, populationData, worldPopulatio
             f.write(f"\n{yearRange} World Population: {countryObjects[yearRange][-1].amount:n}")
             yearRangePopTotal = 0
             for country in range(len(countryObjects[yearRange]) - 1):
-                yearRangePopTotal += countryObjects[yearRange][country].avgPopulation
-                f.write(f"\n{countryObjects[yearRange][country].name} \n Inflation Rate: {countryObjects[yearRange][country].avgInflationRate} \n Population: {countryObjects[yearRange][country].avgPopulation:n}\n ")
+                yearRangePopTotal += countryObjects[yearRange][country].population
+                f.write(f"\n{countryObjects[yearRange][country].name} \n Inflation Rate: {countryObjects[yearRange][country].inflationRate} \n Population: {countryObjects[yearRange][country].population:n}\n ")
 
             worldPop = countryObjects[yearRange][-1].amount
             percentageOfWorldPop = str(round((yearRangePopTotal / worldPop) * 100,2)) + " %"
@@ -67,7 +70,8 @@ def outputDataByAverage(yearRange, inflationData, populationData, worldPopulatio
 
 
 
-def outputDataByYear(yearRange, inflationData, populationData, worldPopulationData):
+def outputDataByYear(yearRange, inflationData, populationData):
+
     countryObjects = createYearRangeDictWithEmptyArrays(yearRange)
 
 
@@ -76,31 +80,27 @@ def outputDataByYear(yearRange, inflationData, populationData, worldPopulationDa
             countryName = str(list(countryInflation.keys())[0])
             countryObj = Country()
             countryObj.setCountryName(countryName)
-            countryObj.setAvgInflationRate(countryInflation[countryName])
+            countryObj.setInflationRate(countryInflation[countryName])
             countryObjects[year].append(countryObj)
                 
 
         for countryPopulation in populationData[year]:
             countryName = str(list(countryPopulation.keys())[0])
-            avgPopulation = countryPopulation[countryName]
+            population = countryPopulation[countryName]
             for country in countryObjects[year]:
                 if countryName == country.name:
-                    country.setAvgPopulation(avgPopulation)
-
-
-        # worldPopObj = worldPopulation()
-        # worldPopObj.setWorldPopulation(worldPopulationData[year])
-        # countryObjects[year].append(worldPopObj)
+                    country.setPopulation(population)
 
     return countryObjects
 
-testObj = outputDataByYear(targetYears, inflationDict, populationDict, worldPopDict)
+
+filteredDataByInflationRate = outputDataByYear(targetYears, inflationDict, populationDict)
 
 
 countriesList = []
 
-for year in testObj:
-        for country in testObj[year]:
+for year in filteredDataByInflationRate:
+        for country in filteredDataByInflationRate[year]:
             countriesList.append(country.name)
            
 countriesList = list(set(countriesList))
@@ -114,20 +114,74 @@ def buildCountryDict(countryList):
     return countryDict
 
 
-countryPopDict = buildCountryDict(countriesList)
-
-for country, pop in countryPopDict.items():
-    for year in testObj:
-        for x in testObj[year]:
+peakPopByCountry = buildCountryDict(countriesList)
+peakYearDict = {}
+for country, pop in peakPopByCountry.items():
+    for year in filteredDataByInflationRate:
+        for x in filteredDataByInflationRate[year]:
             if x.name == country:
-                if x.avgPopulation > pop:
-                    countryPopDict[country] = x.avgPopulation
+                    if x.population > pop:
+                        peakPopByCountry[country] = x.population
+                        peakYearDict[country] = year
 
-print(countryPopDict)
 
 totalPop = 0
-for country, pop in countryPopDict.items():
+for country, pop in peakPopByCountry.items():
     totalPop += pop
 
-print(testObj)
-print(totalPop)
+
+# print(filteredDataByInflationRate)
+# print(peakPopByCountry)
+
+# print(totalPop)
+popualtionInPercent = (totalPop/ 7900000000) * 100
+
+
+# with open('results_new.txt', 'w') as f:
+
+#         for year in filteredDataByInflationRate:
+#             f.write("\n")
+#             for country in filteredDataByInflationRate[year]:
+#                 f.write(f"\n{year}")
+#                 f.write(f"\n{country.name} \n Inflation Rate: {country.avgInflationRate} \n Population: {country.population:n}\n ")
+#         f.write(f"{popualtionInPercent}% of the world's population have experienced inflation over 20% since 1961")
+
+
+# index_list = []
+# for k, v in filteredDataByInflationRate.items():
+#     index_list.append(k)
+
+
+dfDictData = {}
+for year, countries in filteredDataByInflationRate.items():
+    countryNameList, countryInflationRateList, countryPopulationList = [], [], []
+    for country in countries:
+        countryNameList.append(country.name)
+        countryInflationRateList.append(country.inflationRate)
+        countryPopulationList.append(country.population)
+    
+    combinedCountryAttributesList = []
+    combinedCountryAttributesList.append(countryNameList)
+    combinedCountryAttributesList.append(countryInflationRateList)
+    combinedCountryAttributesList.append(countryPopulationList)
+
+    dfDictData[year] = combinedCountryAttributesList
+
+
+listOfDf = []
+finalDf = None
+
+for year, columnsData in dfDictData.items():
+    df = pd.DataFrame(columnsData).T
+    df.columns = ["country", "inflationRate", "population"]
+    df["year"] = year
+
+    listOfDf.append(df)
+
+
+final = pd.concat(listOfDf)
+
+final.set_index(["year", "country"], inplace=True)
+print(final)
+
+
